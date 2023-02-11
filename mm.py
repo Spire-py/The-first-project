295a34244867245bd2eff8661bccdf18d9339de3
Mehrdad <125157647+Spire-py@users.noreply.github.com>
2023-02-12 00:25:20 +0330

    The first project
    The first project

---
+++ mm.py
@@ -0,0 +1,11 @@
+a=(input("code melli:\t"))
+if (a.isnumeric()):
+    print("afarin")
+else:print("number vared kon")
+b=input("email:\t")
+b=b.lower()
+if b.count("yaho")>0:
+    print("yaho nabash")
+else:
+    print("mashalaa")
+    
